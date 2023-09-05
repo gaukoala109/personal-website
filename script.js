64b4bfab1678c76b07d44cdd6d61d5352bce8ebc
Quoc Le <68402920+gaukoala109@users.noreply.github.com>
2023-09-05 00:00:27 -0700

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const btnHamburger = document.getElementsByClassName('hamburger-button')[0];
-const links = document.getElementsByClassName('links')[0];
-
-btnHamburger.addEventListener('click', () => {
-  links.classList.toggle('show');
-})
